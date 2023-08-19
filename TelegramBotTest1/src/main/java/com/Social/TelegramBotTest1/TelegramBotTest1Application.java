9c3aaed8513b018ed9b6768f3113fd9175d03c95
ams32ru <ams32ru@mail.ru>
2023-08-19 14:07:24 +0300

    Initial commit

---
+++ TelegramBotTest1/src/main/java/com/Social/TelegramBotTest1/TelegramBotTest1Application.java
@@ -0,0 +1,13 @@
+package com.Social.TelegramBotTest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotTest1Application.class, args);
+	}
+
+}
---
+++ TelegramBotTest1/src/test/java/com/Social/TelegramBotTest1/TelegramBotTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Social.TelegramBotTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
